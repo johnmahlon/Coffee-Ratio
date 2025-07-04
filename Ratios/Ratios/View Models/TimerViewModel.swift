85bc0b576aead6066a60b60a0db971ebc3965140
jermaine-iris <danieljermaine97@gmail.com>
2025-07-04 10:39:33 +0100

    added haptic function the start

--- Ratios/Ratios/View Models/TimerViewModel.swift
+++ Ratios/Ratios/View Models/TimerViewModel.swift
@@ -14,7 +14,7 @@ class TimerViewModel {
     private var cancellableTimer: Cancellable?
 
     public func start() {
-        
+        HapticManager.shared.vibrate(for: .success)
         cancellableTimer = timer.connect()
     }
 
